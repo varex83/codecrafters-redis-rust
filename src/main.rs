526466323ccfcb8467082cda491cdc650c750b50
Varex Silver <limposfeed@gmail.com>
2023-07-25 12:04:13 +0300

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,21 @@
-use std::io::{BufRead, BufReader, BufWriter, Read, Write};
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
-use bytes::buf::{Reader, Writer};
-
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                let mut reader = BufReader::new(_stream.try_clone().unwrap());
-
-                let mut writer = BufWriter::new(_stream.try_clone().unwrap());
-
-
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let mut buf = [0; 512];
                 loop {
-                    let mut buffer = String::new();
-                    let res = reader.read_line(&mut buffer);
-
-                    if buffer.trim().is_empty() || res.is_err() {
-                        println!("Closing connection");
+                    let read_count = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
                         break;
                     }
-
-                    println!("Received: {}", buffer);
-
-                    let res = writer.write_all(b"+PONG\r\n");
-
-                    if res.is_err() {
-                        println!("Closing connection");
-                        break;
-                    }
-
-                    writer.flush().unwrap();
-
-                    println!("Sent: +OK");
+                    stream.write(b"+PONG\r\n").unwrap();
                 }
-
-
             }
             Err(e) => {
                 println!("error: {}", e);
